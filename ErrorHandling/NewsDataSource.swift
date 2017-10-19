86d4f5dfe4cc7d8d20966f614c150f4b9859b87a
TomerBu <tomerbu@hackerus-Mac.local>
2017-10-19 18:29:42 +0300

    json

--- ErrorHandling/NewsDataSource.swift
+++ ErrorHandling/NewsDataSource.swift
@@ -9,39 +9,39 @@
 import UIKit
 //c554ee8ea96747189054b2a3b3f6c9fc
 class NewsDataSource {
-
+    
     static func getData(){
-        guard let url = URL(string: "https://newsapi.org/v1/sources?language=en") else{return}
+        guard let url = URL(string: "https://newsapi.org/v1/sources?language=en") else {return}
+        let session = URLSession.shared// URLSession(configuration: ...)
         
-        let session = URLSession.shared // the default session is cached. 
-
-        let task = session.dataTask(with: url) { (data, response, error) in
-            //data is binary
-            guard let data = data else{return} //binary
-            
-            //convert the data to json:
-            guard let json = try? JSONSerialization.jsonObject(with: data, options: []) as? Json else{return}
-            
-            if let status = json?["status"] as? String{
-                print(status)
+        let task = session.dataTask(with: url){ (data: Data?, res, err) in
+            if let err = err{
+                print(err)
+                //test the response for debuging aid
+                return
             }
             
-            if let sourcesArray = json?["sources"] as? [Json] {
-                //continue parsing...
-                
-                
-                for source in sourcesArray{
-                    if let id = source["id"] as? String{
-                        print(id)
+            guard let data = data else{print("No data check response");return}
+            //we have data. :binary
+            let json = try? JSONSerialization.jsonObject(with: data, options: [])
+
+            if let json = json as? JSON{
+                if let sourcesArr = json["sources"] as? [JSON]{
+                    for src in sourcesArr{
+                        guard let id = src["id"] as? String,
+                              let name = src["name"] as? String,
+                              let description = src["description"] as? String,
+                              let url = src["url"] as? String else{return}
+                        
+                        let src = NewsSource(id: id, excerpt: description, name: name, url: url)
                     }
                 }
             }
         }
-        //tasks start as suspended
-        
-        
         task.resume()
     }
 }
 
-typealias Json = Dictionary<String, Any>
+
+typealias JSON = [String: Any]
+ 
